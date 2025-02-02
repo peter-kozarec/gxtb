e494c36b4107d3347fed99ccca579e649ae2ec63
Peter Kozarec <peter.kozarec@outlook.sk>
2025-02-02 14:42:41 +0100

    Added api example

---
+++ examples/api_example/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"peter-kozarec/gxtb"
+)
+
+func main() {
+	c := gxtb.NewApiClient(gxtb.DefaultDemoApiOptions())
+	ctx := context.Background()
+
+	if err := c.Connect(ctx); err != nil {
+		log.Fatalf("unable to connect: %v", err)
+	}
+	defer c.Disconnect()
+
+	if _, err := c.Login(ctx, os.Getenv("XTB_UserId"), os.Getenv("XTB_Password"), "testApp"); err != nil {
+		log.Fatalf("unable to login: %v", err)
+	}
+	defer c.Logout(ctx)
+
+	version, err := c.GetVersion(ctx)
+	if err != nil {
+		log.Fatalf("unable to retrieve api version: %v", err)
+	}
+	log.Printf("Api version: %s\n", version)
+
+	serverTime, err := c.GetServerTime(ctx)
+	if err != nil {
+		log.Fatalf("unable to retrieve server time: %v", err)
+	}
+	log.Printf("Server time: %s\n", serverTime.TimeString)
+}
